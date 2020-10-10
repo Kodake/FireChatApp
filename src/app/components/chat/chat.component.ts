a610cfd68e07854794908bd7fe93c75ecf31a318
Kodake <39713700+Kodake@users.noreply.github.com>
2020-10-10 13:15:33 -0400

    All changes about FireChatApp

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from "../../services/chat.service";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  mensaje: string = "";
+  elemento: any;
+
+  constructor(public _chatService: ChatService) {
+    this._chatService.cargarMensajes()
+      .subscribe(() => {
+        setTimeout(() => {
+          
+        }, 20);
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      });
+  }
+
+  ngOnInit(): void {
+    this.elemento = document.getElementById('app-mensajes');
+  }
+
+  enviar_mensaje() {
+    console.log(this.mensaje);
+
+    if (this.mensaje.length === 0) {
+      return;
+    }
+
+    this._chatService.agregarMensaje(this.mensaje)
+      .then(() => this.mensaje = "")
+      .catch((err) => console.log('Error al enviar', err));
+
+  }
+
+}
